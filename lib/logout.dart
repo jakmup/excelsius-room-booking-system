1c03884f0cec52ab9965239a123cdfbbc601f448
indi3mi <nikilhami@graduate.utm.my>
2023-05-07 21:12:08 +0800

    login

--- lib/logout.dart
+++ lib/logout.dart
@@ -7,40 +7,40 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   void _logout() {
-  // TODO: Clear any locally stored user session information here.
-  // For example, you might use a package like shared_preferences to store and retrieve
-  // user data from local storage.
+    // TODO: Clear any locally stored user session information here.
+    // For example, you might use a package like shared_preferences to store and retrieve
+    // user data from local storage.
 
-  // Navigate the user to the login screen.
-  Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
-}
+    // Navigate the user to the login screen.
+    Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+  }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('My App'),
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Excelsius Room Booking System',
-            style: TextStyle(fontSize: 24.0),
-          ),
-          SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: _logout,
-            style: ElevatedButton.styleFrom(
-              primary: Colors.red, // Change the button's background color.
-              onPrimary: Colors.white, // Change the button's text color.
-            ),
-            child: Text('Logout'),
-          ),
-        ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
       ),
-    ),
-  );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Excelsius Room Booking System',
+              style: TextStyle(fontSize: 24.0),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _logout,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red, // Change the button's background color.
+                onPrimary: Colors.white, // Change the button's text color.
+              ),
+              child: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
-
